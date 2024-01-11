ab9afb92b04d1a16b2485dd25d9145220aa1e407
Animxer18 <138975524+Animxer18@users.noreply.github.com>
2024-01-11 10:58:10 +0800

    Update request.ts

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,5 @@
 import getJSON from './api';
-const BASE_URL = 'https://cors.proxy.consumet.org';
+const BASE_URL = 'https://animxer-api-cvxg.vercel.app/';
 
 const api = {
   popularAnime: (page: string | number): Promise<any> => {
