66e53697606c8327eb09924558c347a7381d4a72
Animxer18 <138975524+Animxer18@users.noreply.github.com>
2024-01-11 10:47:30 +0800

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.consumet.org';
+export const BASE_URL = 'https://animxer-api-cvxg.vercel.app/';
 export const PAGE = 1;
 export const CORS_PROXY = 'https://cors.proxy.consumet.org/';
 export const GOGO_PROVIDER = 'gogoanime';
